005615b5af4cf796fca77ac47a4a7d622a8900a3
gadiga <jayguru68@gmail.com>
2018-11-29 09:10:34 -0500

    AWS Hello world first commit

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,16 @@
+/**
+ * Created by mattua on 23/07/2016.
+ */
+public class Credentials {
+
+
+    public static final String access_key_id= "AKIAJQBA6TTUVSE7ROHA";
+
+    public static final String secret_access_key ="<>";
+    
+    public static final String aws_profile = "gadiga";
+
+
+
+}
+
